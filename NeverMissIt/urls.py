009d5492f048fc5e366fa39cc0ccc29ac6a1a178
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-05-18 03:03:17 +0530

    silly commit. added namespace (app_name) in the urls.py

--- NeverMissIt/urls.py
+++ NeverMissIt/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'NeverMissIt'
+
 urlpatterns = [
     #path('home/', views.homepage, name='homepage'),
     path('', views.homepage, name='homepage'),
